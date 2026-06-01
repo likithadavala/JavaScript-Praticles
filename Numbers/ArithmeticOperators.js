0439ed07770e47c668b47fd78ff636564f37a12e
Likithadavala <likithadavala99@gmail.com>
2026-06-01 22:44:34 +0530

    Arithmetic Operations

---
+++ Numbers/ArithmeticOperators.js
@@ -0,0 +1,8 @@
+function addNumbers(){
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstNumber + secondNumber;
+    alert(sum);
+    
+}
