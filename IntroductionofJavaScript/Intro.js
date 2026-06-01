61064b6b4efaa2ec85c097b65dee4289f1b7257a
Likithadavala <likithadavala99@gmail.com>
2026-06-01 22:12:24 +0530

    Introduction of javascript

---
+++ IntroductionofJavaScript/Intro.js
@@ -0,0 +1,3 @@
+alert("Introduction to pragramming");
+console.log("What is JavaScript?")
+console.log("JavaScript is a programming language primarily used to make websites interactive and dynamic.")
