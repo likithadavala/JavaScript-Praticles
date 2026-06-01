cc8c807447e54257e0711c81d458cda332be94d4
Likithadavala <likithadavala99@gmail.com>
2026-06-01 23:09:08 +0530

    Arithematic opertions

--- Numbers/ArithmeticOperators.js
+++ Numbers/ArithmeticOperators.js
@@ -6,3 +6,37 @@ function addNumbers(){
     alert(sum);
     
 }
+function subNumbers(){
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstNumber + secondNumber;
+    alert(sum);
+    
+}
+function mulNumbers(){
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstNumber + secondNumber;
+    alert(sum);
+    
+}
+function divNumbers(){
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstNumber + secondNumber;
+    alert(sum);
+    
+}
+function remNumbers(){
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstNumber + secondNumber;
+    alert(sum);
+    
+}
+
+
